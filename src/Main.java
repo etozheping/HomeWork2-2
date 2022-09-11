f064cc6783e8e0ab1c19fe44052ef689feb9cf11
etozheping <krashsamara@gmail.com>
2022-09-11 13:58:57 +0400

    Решение задачи №6

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        var BoxerWeight1 = 78.2;
+        var BoxerWeight2 = 82.7;
+        var allWeightBoxer = BoxerWeight1 + BoxerWeight2;
+        System.out.println("Общий вес бойцов " + allWeightBoxer + " кг");
+        var differenceWeightBoxer = BoxerWeight2 - BoxerWeight1;
+        System.out.println("разница в весе у бойцов " + differenceWeightBoxer + " кг");
+
+    }
+}
