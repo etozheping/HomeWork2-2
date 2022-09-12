194f314c72e096ebae79f66d5f93c600e96c6f56
etozheping <krashsamara@gmail.com>
2022-09-12 06:05:42 +0400

    Решение задачи №7

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
         var BoxerWeight1 = 78.2; // Вес боксера 1
         var BoxerWeight2 = 82.7; // Вес боксера 2
-        //var allWeightBoxer = BoxerWeight2 + BoxerWeight1; // allWeightBxer - общий вес боксеров
+        var allWeightBoxer = BoxerWeight2 + BoxerWeight1; // allWeightBxer - общий вес боксеров
 
         //Решение 6 задания
         //System.out.println("Общий вес бойцов " + allWeightBoxer + " кг");
@@ -12,11 +12,14 @@ public class Main {
         //System.out.println("разница в весе у бойцов " + differenceWeightBoxer + " кг");
 
         //Решение 7 задания
-        //var differenceWeightBoxer = (BoxerWeight2 - BoxerWeight1) % allWeightBoxer; // differenceWeightBoxer - разница в весе боксёров.
-        //System.out.println("разница в весе у бойцов " + differenceWeightBoxer + " кг!");
+        var differenceWeightBoxer = (BoxerWeight2 - BoxerWeight1) % allWeightBoxer; // differenceWeightBoxer - разница в весе боксёров.
+        System.out.println("разница в весе у бойцов " + differenceWeightBoxer + " кг!"); //решение задачи с помощью %
 
-        var differenceWeightBoxer = BoxerWeight2 - BoxerWeight1;
-        System.out.println("Разница в весе у бойцов " + differenceWeightBoxer + " кг!");
+        //var differenceWeightBoxer = BoxerWeight2 - BoxerWeight1;
+        //System.out.println("Разница в весе у бойцов " + differenceWeightBoxer + " кг!"); //решение задачи с помощью вычитания
+
+
+
 
 
 
